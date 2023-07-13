81cbefe3c7108f6b3b961797650bccc53766a108
darichcastle <ben4alignment@gmail.com>
2023-07-13 10:54:05 -0400

    a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * This prograprints exactly "Programming is like building a multilingual puzzle
+ * i will only use puts to print
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+}
