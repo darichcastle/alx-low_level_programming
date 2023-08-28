57323ebe68f5dc02820c811589a83c01903a83e2
darichcastle <ben4alignment@gmail.com>
2023-08-28 21:46:00 +0100

    added task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 
+
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
  *
- * Return: number of nodes
+ * Return: pointer to the new node, or NULL if it fails
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
