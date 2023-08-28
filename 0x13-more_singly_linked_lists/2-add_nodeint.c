ce0222030c81f3cdd560fe26060bae801b19c43b
darichcastle <ben4alignment@gmail.com>
2023-08-28 22:07:00 +0100

    added task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,9 +11,11 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL) {
-        return NULL;
-    }
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+		}
 
 	new_node->n = n;
 
