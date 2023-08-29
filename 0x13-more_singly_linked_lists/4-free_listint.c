64ce7babf864ac7b246e177ca64e9792695f75a3
darichcastle <ben4alignment@gmail.com>
2023-08-29 12:15:26 +0100

    added task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a linked list
+ * @head: - Pointer to the first element to be freed
+ * current - points to the headis a pointer that starts at the head of the list
+ * to_return - temporary pointer that holds the address of the current
+ * node before we move to the next one
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *to_return;
+
+	while (current != NULL)
+	{
+		to_return = current;
+		current = current->next;
+		free(to_return);
+	}
+}
