e0ffec90046999f8569d53fd66f29fdd38513e8a
darichcastle <ben4alignment@gmail.com>
2023-09-28 23:22:50 +0200

    added task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - convert a binary number to an unsigned int
- * @binary_str: char string containing binary digits
+ * @b: char string containing binary digits
  * Return: converted decimal number or 0 if there are invalid characters
  */
 unsigned int binary_to_uint(const char *b)
@@ -11,19 +11,19 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal_total = 0, binary_power = 1;
 	int binary_length;
 
-	if (binary_str == NULL)
+	if (b == NULL)
 		return (0);
 
-	for (binary_length = 0; binary_str[binary_length]; binary_length++)
+	for (binary_length = 0; b[binary_length]; binary_length++)
 	{
-		if (binary_str[binary_length] != '0' && binary_str[binary_length] != '1')
+		if (b[binary_length] != '0' && b[binary_length] != '1')
 			return (0);
 	}
 
 	for (binary_power = 1, decimal_total = 0, binary_length--;
 		binary_length >= 0; binary_length--, binary_power *= 2)
 	{
-		if (binary_str[binary_length] == '1')
+		if (b[binary_length] == '1')
 			decimal_total += binary_power;
 	}
 
